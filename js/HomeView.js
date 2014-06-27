cb2b1b0ebf91afadaabd2073a0658d1f03223e64
Örs Kovács <orskovacs92@gmail.com>
2014-06-27 11:43:30 +0200

    tapped

--- js/HomeView.js
+++ js/HomeView.js
@@ -6,17 +6,10 @@ var HomeView = function(store) {
 	};
 	
 	this.findByName = function() {
-    store.findByName($('.search-key').val(), function(employees) {
-        $('.employee-list').html(HomeView.liTemplate(employees));
-        if (self.iscroll) {
-            console.log('Refresh iScroll');
-            self.iscroll.refresh();
-        } else {
-            console.log('New iScroll');
-            self.iscroll = new iScroll($('.scroll', self.el)[0], {hScrollbar: false, vScrollbar: false });
-        }
-    });
-};
+		store.findByName($('.search-key').val(), function(employees) {
+			$('.employee-list').html(HomeView.liTemplate(employees));
+		});
+	};
  
     this.initialize = function() {
         // Define a div wrapper for the view. The div wrapper is used to attach events.
