777022f5f6ff96dc02e8eea380ffc58ec555b526
Örs Kovács <orskovacs92@gmail.com>
2014-06-27 11:28:56 +0200

    style

--- js/main.js
+++ js/main.js
@@ -1,23 +1,12 @@
 var app = {
+    
 
-    findByName: function() {
-        console.log('findByName');
-        this.store.findByName($('.search-key').val(), function(employees) {
-            var l = employees.length;
-            var e;
-            $('.employee-list').empty();
-            for (var i=0; i<l; i++) {
-                e = employees[i];
-                $('.employee-list').append('<li><a href="#employees/' + e.id + '">' + e.firstName + ' ' + e.lastName + '</a></li>');
-            }
-        });
-    },
 
-    initialize: function() {
-        this.store = new MemoryStore();
-        $('.search-key').on('keyup', $.proxy(this.findByName, this));
-    }
-
+initialize: function() {
+    var self = this;
+    this.store = new MemoryStore(function() {
+        $('body').html(new HomeView(self.store).render().el);
+    });
+}
 };
-
 app.initialize();
