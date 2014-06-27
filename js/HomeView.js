ff2a04f3c41372235f690829838ad86a4934fed8
Örs Kovács <orskovacs92@gmail.com>
2014-06-27 12:09:47 +0200

    scroll

--- js/HomeView.js
+++ js/HomeView.js
@@ -6,10 +6,20 @@ var HomeView = function(store) {
 	};
 	
 	this.findByName = function() {
-		store.findByName($('.search-key').val(), function(employees) {
-			$('.employee-list').html(HomeView.liTemplate(employees));
-		});
-	};
+    store.findByName($('.search-key').val(), function(employees) {
+        $('.employee-list').html(HomeView.liTemplate(employees));
+        if (self.iscroll) {
+            console.log('Refresh iScroll');
+            self.iscroll.refresh();
+        } else {
+            console.log('New iScroll');
+            self.iscroll = new iScroll($('.scroll', self.el)[0], {hScrollbar: false, vScrollbar: false });
+        }
+		setTimeout( function(){
+                      myScroll.refresh() ;
+                    } , 100 ) ;
+    });
+};
  
     this.initialize = function() {
         // Define a div wrapper for the view. The div wrapper is used to attach events.
